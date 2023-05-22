f37ba029328174c48f2ebd879fb980cbc05aed4f
Rafal Songin <rafal.songin@gmail.com>
2023-05-22 17:49:31 +0200

    Create Login.cs

---
+++ ChapeauModel/Login.cs
@@ -0,0 +1,11 @@
+﻿namespace ChapeauModel
+{
+    public class Login
+    {
+        void PrintHello()
+        {
+            Console.WriteLine("Rafal: It's a me Cezar");
+            Console.WriteLine("Rafal: It's a me Miguel");
+        }
+    }
+}
